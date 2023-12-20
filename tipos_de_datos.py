4c2034beb483b9e5b5c47c1d7c940fe31a5fa07f
Simon Guzman <lcdosguzman@gmail.com>
2023-12-20 20:31:30 -0300

    add tipos de datos

---
+++ tipos_de_datos.py
@@ -0,0 +1,33 @@
+#Tipos de datos básicos en Python
+
+#Entero
+#Representan números enteros sin parte decimal.
+entero = 42
+
+#Float
+#Representan números con parte decimal.
+float = 3.14
+
+#Cadenas o string
+#Representan secuencias de caracteres.
+#Pueden definirse con comillas simples o dobles.
+cadena = "Esto es un texto"
+
+#Booleano
+#Representan valores de verdad, True (verdadero) o False (falso).
+booleano = True
+
+#Listas
+lista = [1,2,3,4,5,6,7]
+
+#Tuplas
+tupla = (1,2,3)
+
+#Conjunto
+conjunto = {1,2,3,4}
+
+#Diccionario
+diccionario = {'clave1': 'valor1', 'clave2': 'valor2'}
+
+#Null o None
+nulo = None
